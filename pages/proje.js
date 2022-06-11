048d5eabf97ba0863f18f133ab23f26c32659d61
muhammedonen <mufaah80@gmail.com>
2022-06-11 17:27:50 +0300

    getStatic

--- pages/proje.js
+++ pages/proje.js
@@ -1,12 +1,26 @@
 import Layout from "../component/layout";
 
-function proje() {
+function proje({name,ders}) {
   return (
     <Layout>
-     <div className="h-32 text-lg font-semibold font-serif flex justify-center items-center text-white">
-     <span>to be continued...</span>
+     <div className="h-32 text-lg font-semibold  flex justify-center items-center text-white">
+     
+     <span className="font-bold text-white ">{name}</span>
+     <span className="font-bold text-red-400 underline ml-1">{ders}</span>
      </div>
     </Layout>
   );
 }
 export default proje;
+export const getStaticProps = async ()=>{
+ const name = "Öğrenilen konu =>";
+ const ders = " getStaticProps";
+
+ return{
+  props: {
+    name,
+    ders
+
+  }
+}
+}
