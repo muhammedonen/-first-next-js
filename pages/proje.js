e629535ef3c936da70e8638bb5a9dda40473cc6c
muhammedonen <mufaah80@gmail.com>
2022-06-12 14:51:34 +0300

    getStaticProps

--- pages/proje.js
+++ pages/proje.js
@@ -1,25 +1,34 @@
 import Layout from "../component/layout";
 
-function proje({name,ders}) {
+function proje({veri,name,subject}) {
   return (
     <Layout>
-     <div className="h-32 text-lg font-semibold  flex justify-center items-center text-white">
-     
-     <span className="font-bold text-white ">{name}</span>
-     <span className="font-bold text-red-400 underline ml-1">{ders}</span>
+     <div className="h-120  flex  flex-col justify-between items-center text-white ">
+        <div className="flex text-center bg-gray-700 p-3 rounded-lg border-2">
+          <h3 className="text-3xl">{subject}</h3>
+          <span className="text-3xl underline text-red-500"><a href="https://jsonplaceholder.typicode.com/users"> {name}</a> </span>
+          </div>
+     {veri.map(e =>(
+
+      <div key={e.id} className="w-60 text-center border-2 m-2 p-2 rounded-lg shadow-md shadow-yellow-300" >
+        <h3 className="text-lg text-red-500 font-semibold p-2">{e.name}</h3>
+        <p>{e.email}</p>
+        <span className="bg-gray-400 border-2 text-center border-yellow-400 text-blue-700 rounded-lg "><b className="font-bold">City :</b> {e.address['city']}</span>
+      </div>
+     ))}
      </div>
     </Layout>
   );
 }
 export default proje;
 export const getStaticProps = async ()=>{
- const name = "Öğrenilen konu =>";
- const ders = " getStaticProps";
-
+  const subject = "Subject =>"
+  const name = "getStaticProps"
+ const res = await fetch('https://jsonplaceholder.typicode.com/users')
+ const data = await res.json();
  return{
   props: {
-    name,
-    ders
+    veri: data,name,subject
 
   }
 }
